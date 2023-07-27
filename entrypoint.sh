c8d901d16124a9b63628b6f44b69f665748eb7a0
Cristiano Nascimeno <Cristiano@proserv.net.br>
2023-07-27 19:32:42 -0300

    fix format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 cd /app
 echo "Starting migration"
-npx prisma migrate deploy 
+npx prisma migrate deploy
 echo "end migration"
+
 echo "Starting building"
 npx tsc
 echo "end building"
+
 echo "Starting server!!!"
 node /app/build/server.js
-
